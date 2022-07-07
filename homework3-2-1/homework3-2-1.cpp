b5dd47fe1d5393010cd9ac02e2f43c52ce423544
Abrroo <akhmetov.ersyn@gmail.com>
2022-07-07 15:51:59 +0300

    gg

--- homework3-2-1/homework3-2-1.cpp
+++ homework3-2-1/homework3-2-1.cpp
@@ -1,20 +1,51 @@
-﻿// homework3-2-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+enum class months
+{
+	null,
+	January, February, March,
+	April, May, June, July,
+	August, September, October,
+	November, December
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+
+	int month = 0;
+	months enum_month = months::null;
+
+	do
+	{
+		std::cout << "Введите номер месяца: ";
+		std::cin >> month;
+		if (month > 13 || month <= -1)
+		{
+			std::cout << "Нет такого месяца!" << std::endl;
+		}
+		else
+		{
+			enum_month = static_cast<months>(month);
+			switch (enum_month)
+			{
+			case months::January:	std::cout << "Январь" << std::endl; break;
+			case months::February:	std::cout << "Февраль" << std::endl; break;
+			case months::March:		std::cout << "Март" << std::endl; break;
+			case months::April:		std::cout << "Апрель" << std::endl; break;
+			case months::May:		std::cout << "Май" << std::endl; break;
+			case months::June:		std::cout << "Июнь" << std::endl; break;
+			case months::July:		std::cout << "Июль" << std::endl; break;
+			case months::August:	std::cout << "Август" << std::endl; break;
+			case months::September:	std::cout << "Сентябрь" << std::endl; break;
+			case months::October:	std::cout << "Октябрь" << std::endl; break;
+			case months::November:	std::cout << "Ноябрь" << std::endl; break;
+			case months::December:	std::cout << "Декабрь" << std::endl; break;
+			case months::null:		break;
+			default:				break;
+			}
+		} 
+	} while (month);
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
